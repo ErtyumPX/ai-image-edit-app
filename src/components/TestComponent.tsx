f3ed6956ab4f51833ab5f45747627d26903034c5
Erthium <ertyum35@gmail.com>
2024-02-10 23:37:40 +0300

    Create a test env to send file to api

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,7 +1,34 @@
+'use client';
+
+import { ApiService } from "@/services/ApiService";
+import { useState } from "react";
+import path from "path";
+
+const imagePath = 'test_image.png';
+
 export default function TestComponent() {
+  const [imageUrl, setImageUrl] = useState<string>('');
+
+  const handleOnClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    // upload image from src/data/test_image.png
+    console.log(imagePath);
+    const response = await fetch(imagePath);
+    if (!response.ok) {
+      throw new Error('Failed to fetch image');
+    }
+    const blob = await response.blob();
+    const file = new File([blob], 'example.png');
+    const prompt = 'Test Prompt';
+    const result = await ApiService.sendEditRequest(file, prompt);
+    //setImageUrl(result);
+    console.log(result);
+  }
+
   return (
     <main>
       <h1>Test Component</h1>
+      <button onClick={handleOnClick}>Test Me</button>
+      <img src={imageUrl} alt="Test Image" />
     </main>
   );
 }
---
+++ src/services/ApiService.ts
@@ -0,0 +1,64 @@
+const BASE_URL = 'http://localhost:3000';
+
+interface FetchOptions {
+    method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
+    data?: object | File;
+}
+
+const JSONheaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json'
+};
+
+const fetchData = async (endpoint: string, method: string, body?: any, headers?: HeadersInit) => {
+    try {
+        const url = `${BASE_URL}/${endpoint}/`;
+        const fetchOptions: RequestInit = {
+            method,
+        };
+        if (headers) {
+            fetchOptions.headers = headers;
+        }
+        if (body) {
+            fetchOptions.body = body;
+        }
+
+        const response = await fetch(url, fetchOptions);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+    
+        const contentType = response.headers.get('content-type');
+        if (contentType && contentType.includes('application/json')) {
+            return await response.json();
+        } else {
+            // If not JSON, assume it's plain text and return it as is
+            return await response.text();
+        }
+    } 
+    catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+};
+
+
+export class ApiService {
+    static async testConnection() {
+        return fetchData('', 'GET', {header: JSONheaders});
+    }
+
+    static async testAiConnection() {
+        return fetchData('editor/test', 'GET', {header: JSONheaders});
+    }
+
+    static async sendEditRequest(image: File, prompt: string) {
+        const formData = new FormData();
+        formData.append('image', image);
+        formData.append('prompt', prompt);
+        const headers = new Headers();
+        headers.append('Access-Control-Allow-Origin', '*');
+        return fetchData('editor/edit', 'POST', formData, headers);
+    }
+}
