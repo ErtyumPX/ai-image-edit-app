80cf5da1aafa44068e3d2eca12d3ad793812b79e
Erthium <ertyum35@gmail.com>
2024-02-17 23:51:36 +0300

    Clean up the dynamic [id] page

--- src/pages/view/[id].tsx
+++ src/pages/view/[id].tsx
@@ -4,82 +4,95 @@ import { useEffect, useState } from 'react';
 
 const PhotoPage = () => {
   const router = useRouter();
-  const { id } = router.query;
+  const id: string = router.query.id as string;
   const [rawImage64, setRawImage64] = useState<string | undefined>(undefined);
   const [editedImage64, setEditedImage64] = useState<string | undefined>(undefined);
   const [isAbleToShare, setIsAbleToShare] = useState(false);
 
   useEffect(() => {
     setIsAbleToShare(!!navigator.share);
-    // fetchPhotosById();
-  }, []);
-
-  const fetchPhotosById = async () => {
+    console.log(router.query);
     try {
+      setTimeout(fetchPhotosById, 1000, router.query.id as string)
+    }
+    catch {
+      
+    }
+    //fetchPhotosById(id);
+  }, [router]);
+
+  const fetchPhotosById = async (id: string) => {
+    try {
+      console.log("Id in Fetch: ", id);
       const pattern = new RegExp("[a-zA-Z0-9]{10}");
       if (!pattern.test(id as string)) {
         console.error('Invalid ID: ', id);
         throw new Error('Invalid ID');
       }
       const requestedEditedImage64 = await ApiService.getImageRequest(id as string, "edited");
-      const editedImageWithMetaData = 'data:image/png;base64,' + requestedEditedImage64;
-
       const requestedRawImage64 = await ApiService.getImageRequest(id as string, "raw");
+      const editedImageWithMetaData = 'data:image/png;base64,' + requestedEditedImage64;
       const rawImageWithMetaData = 'data:image/png;base64,' + requestedRawImage64;
       setEditedImage64(editedImageWithMetaData);
       setRawImage64(rawImageWithMetaData);
+
     } 
     catch (error) {
       console.error('Error fetching photos:', error);
     }
   };
 
-  const downloadImage = () => {
-    if (editedImage64) {
-      const link = document.createElement('a');
-      link.href = editedImage64;
-      link.download = 'image.jpg';
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
+  const downloadImage = (imageIn64: string) => {
+    const link = document.createElement('a');
+    link.href = imageIn64;
+    link.download = 'image.jpg';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
 
-  const shareImage = async () => {
-    if (editedImage64) {
-      if (navigator.share) {
-        try {
-          await navigator.share({
-            title: 'Image Share',
-            text: 'Check out this image',
-            url: editedImage64,
-          });
-        } catch (error) {
-          console.error('Error sharing:', error);
-        }
+  const shareImage = async (imageIn64: string) => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: 'Image Share',
+          text: 'Check out this image',
+          url: imageIn64,
+        });
       } 
-      else {
-        console.log('Sharing not supported');
-        // We may try something else here...
+      catch (error) {
+        console.error('Error sharing:', error);
       }
+    } 
+    else {
+      console.log('Sharing not supported');
+      // We may try something else here...
     }
   }
 
   return (
     <div>
-      <h1>Photos for ID: {id}</h1>
-      {editedImage64 && rawImage64 ?
+      <h1>Photos for ID: {router.query.id}</h1>
+      {rawImage64 &&
         <>
-          <img src={editedImage64} alt="Edited Snapshot" />
           <img src={rawImage64} alt="Snapshot" />
-          <button onClick={downloadImage}> Download </button>
+          <button onClick={() => downloadImage(rawImage64)}> Download </button>
           {isAbleToShare && 
-            <button onClick={shareImage}> Share </button>
+            <button onClick={() => shareImage(rawImage64)}> Share </button>
           }
         </>
-        : <button onClick={fetchPhotosById}> Fetch Photos </button>
       }
-    </div>
+      {editedImage64 &&
+        <>
+          <img src={editedImage64} alt="Edited Snapshot" />
+          <button onClick={() => downloadImage(editedImage64)}> Download </button>
+          {isAbleToShare && 
+            <button onClick={() => shareImage(editedImage64)}> Share </button>
+          }
+        </>
+      }
+      <button onClick={() => fetchPhotosById(router.query.id as string)}> Fetch Photos </button>
+      </div>
   );
 };
 
